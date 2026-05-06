e9ac5e0433dc59393481eeffe3db969c60f22b9b
Noureddinechakroun <noureddinechakroun9@gmail.com>
2026-05-06 12:12:11 +0100

    finallllll

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,27 @@ Route::redirect('/admin', '/admin/dashboard');
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin');
-    Route::resource('users',     UserController::class)->except(['show']);
-    Route::resource('voitures',  VoitureController::class)->except(['show']);
-    Route::resource('locations', LocationController::class)->except(['show']);
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    Route::get('/voitures', [VoitureController::class, 'index'])->name('voitures.index');
+    Route::get('/voitures/create', [VoitureController::class, 'create'])->name('voitures.create');
+    Route::post('/voitures', [VoitureController::class, 'store'])->name('voitures.store');
+    Route::get('/voitures/{voiture}/edit', [VoitureController::class, 'edit'])->name('voitures.edit');
+    Route::put('/voitures/{voiture}', [VoitureController::class, 'update'])->name('voitures.update');
+    Route::delete('/voitures/{voiture}', [VoitureController::class, 'destroy'])->name('voitures.destroy');
+
+    Route::get('/locations', [LocationController::class, 'index'])->name('locations.index');
+    Route::get('/locations/create', [LocationController::class, 'create'])->name('locations.create');
+    Route::post('/locations', [LocationController::class, 'store'])->name('locations.store');
+    Route::get('/locations/{location}/edit', [LocationController::class, 'edit'])->name('locations.edit');
+    Route::put('/locations/{location}', [LocationController::class, 'update'])->name('locations.update');
+    Route::delete('/locations/{location}', [LocationController::class, 'destroy'])->name('locations.destroy');
 });
 
 Route::middleware('auth')->group(function () {
